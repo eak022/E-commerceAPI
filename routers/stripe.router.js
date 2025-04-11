e61aeb186152047c0d551c6e0fc0fb5a7a91b59a
eak022 <121035260+eak022@users.noreply.github.com>
2025-04-11 16:47:14 +0700

    Update stripe.router.js

--- routers/stripe.router.js
+++ routers/stripe.router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const stripeController = require("../controllers/stripe.controller");
 
 router.post("/create-checkout-session", stripeController.createCheckOutSession);
-router.post("/webhook", stripeController.webhook); 
+router.post("/", stripeController.webhook); 
 
 module.exports = router;
